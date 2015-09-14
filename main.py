feb32ac23a50a04dc8c1e403a960c531f32ef9b0
Sebastjan Koci <sebastjan.koci@gmail.com>
2015-09-14 20:37:34 +0200

    dodano leto 2015

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from google.appengine.api import users
 from google.appengine.api import urlfetch
 from models import Salary
 from BasicHandlers import MainHandler, DodajHandler, DodanoHandler, TableHandler, TableDelete
-from Pregled2015 import L2015Handler, Januar15Handler, Februar15Handler
+from Pregled2015 import L2015Handler, Januar15Handler, Februar15Handler, Marec15Handler, April15Handler, Maj15Handler,\
+    Junij15Handler, Julij15Handler, Avgust15Handler, September15Handler, Oktober15Handler, November15Handler,\
+    December15Handler
 
 
 app = webapp2.WSGIApplication([
@@ -19,4 +21,14 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/l2015', L2015Handler,  name="pregled"),
     webapp2.Route('/januar15', Januar15Handler),
     webapp2.Route('/februar15', Februar15Handler),
+    webapp2.Route('/marec15', Marec15Handler),
+    webapp2.Route('/april15', April15Handler),
+    webapp2.Route('/maj15', Maj15Handler),
+    webapp2.Route('/junij15', Junij15Handler),
+    webapp2.Route('/julij15', Julij15Handler),
+    webapp2.Route('/avgust15', Avgust15Handler),
+    webapp2.Route('/september15', September15Handler),
+    webapp2.Route('/oktober15', Oktober15Handler),
+    webapp2.Route('/november15', November15Handler),
+    webapp2.Route('/december15', December15Handler),
 ], debug=True)
